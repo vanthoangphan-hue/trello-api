e90f793251f09555262d152caca67d4ef5c54684
TRANNGUYENPHUONGANH <phuonganhtrannguyen172@gmail.com>
2026-04-13 13:57:53 +0700

    Init code

---
+++ src/server.js
@@ -0,0 +1,13 @@
+//const express = require('express')
+import express from 'express'
+const app = express()
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', function (req, res){
+    res.send('<h1>Hello World tran nguyen phuong anh!</h1>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello panhdev,chạy http://${hostname}:${port}/`)
+})
